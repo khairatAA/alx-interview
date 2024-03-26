b54cd7acdbfe318410ba596437470c003dcbb0c2
Khairat Adesina <khairatadesina01@gmail.com>
2024-03-26 18:16:31 +0100

    fix: Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,6 @@
 """Log parsing task"""
 import sys
 import re
-import signal
 
 
 def log_parsing():
@@ -18,6 +17,7 @@ def log_parsing():
     input_pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] '\
         r'"GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
     total_file_size = 0
+    line_count = 0
 
     status_code = {
         200: 0,
@@ -31,7 +31,7 @@ def log_parsing():
     }
 
     try:
-        for i, line in enumerate(sys.stdin, start=1):
+        for line in sys.stdin:
             match = re.match(input_pattern, line)
             if match:
                 file_size = int(match.group(4))
@@ -43,16 +43,29 @@ def log_parsing():
                     if file_code == code:
                         status_code[code] += 1
 
-                if i % 10 == 0:
-                    print(f'File size: {total_file_size}')
-                    for code, count in sorted(status_code.items()):
-                        if count > 0:
-                            print(f'{code}: {count}')
-    except KeyboardInterrupt:
-        print(f'File size: {total_file_size}')
-        for code, count in sorted(status_code.items()):
-            if count > 0:
-                print(f'{code}: {count}')
+                line_count += 1
+                if line_count % 10 == 0:
+                    print_stat(total_file_size, status_code)
+                    total_file_size = 0
+                    status_code = {
+                        200: 0,
+                        301: 0,
+                        400: 0,
+                        401: 0,
+                        403: 0,
+                        404: 0,
+                        405: 0,
+                        500: 0,
+                    }
+    except KeyboardInterrupt:     
+        print_stat(total_file_size, status_code)
+
+
+def print_stat(total_file_size, status_code):
+    print(f'File size: {total_file_size}')
+    for code, count in sorted(status_code.items()):
+        if count > 0:
+            print(f'{code}: {count}')
 
 
 if __name__ == "__main__":
