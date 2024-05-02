3a1a891e42dd1119f8021cd1eaa03b60817275db
Khairat Adesina <khairatadesina01@gmail.com>
2024-05-02 22:07:10 +0100

    add: Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!usr/bin/python3
+"""Change comes from within"""
+
+
+def makeChange(coins, total):
+    """
+    makeChange: Change comes from within
+    Args:
+    coins: the coins.
+    total: total amount coins
+    """
+
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
